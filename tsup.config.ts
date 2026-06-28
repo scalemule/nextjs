00862788039467e955655309af97f2e897d56470
Plamen <plamen@tribedojo.com>
2026-06-28 07:02:24 -0700

    fix: externalize SDK in Next.js build

--- tsup.config.ts
+++ tsup.config.ts
@@ -15,8 +15,7 @@ export default defineConfig({
   splitting: false,
   sourcemap: false,
   clean: true,
-  noExternal: ['@scalemule/sdk'],
-  external: ['react', 'react-dom', 'next', 'next/headers', 'next/server'],
+  external: ['@scalemule/sdk', 'react', 'react-dom', 'next', 'next/headers', 'next/server'],
   treeshake: true,
   minify: false,
 })
